8bc930df566be778c91c78e469a7ec8eedda3ebe
moisesguillen <moisesguillenn@gmail.com>
2025-06-03 20:58:30 -0400

    Cool Temperature Converter !1!!

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 #include "getInput.h"
 
 int main() {
-    std::cout << "-------------------------------------------\n";
-    std::cout << "          TEMPERATURE CONVERTER    v1.0        \n";
-    std::cout << "-------------------------------------------\n";
     double x { getInput() };
     std::cout << "Equivalent temperature: " << convertTemp(x) << " (°C)\n";
     return 0;
